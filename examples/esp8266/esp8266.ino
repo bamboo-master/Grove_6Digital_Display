4b834b3099b486e314eccd5af424c7877fda9c2f
bamboo-master <81521477+bamboo-master@users.noreply.github.com>
2023-03-27 11:19:24 +0300

    Update esp8266.ino

--- examples/esp8266/esp8266.ino
+++ examples/esp8266/esp8266.ino
@@ -1,50 +1,49 @@
-#include "TM1637.h"
-#include "Ticker.h"
-
-
+#include <TM1637.h>
+#include <Ticker.h>
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+const char *ssid     = "???";
+const char *password = "???";
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP,"ntp.ix.ru", 10800);
 int8_t TimeDisp[] = {0x00,0x00,0x00,0x00,0x00,0x00};
-
-
 #define CLK 12 //D6  GPIO12
 #define DIO 13 //D7  GPIO13
-
 TM1637 tm1637(CLK,DIO);
-
 Ticker points;
 boolean point;
-
-unsigned long h, m, s, timing;
+unsigned long timing;
 
 void setup() {
-  
-  tm1637.set(BRIGHTEST);          
-  tm1637.init();          
+tm1637.set(BRIGHT_TYPICAL);          
+tm1637.init();          
 
+WiFi.begin(ssid, password);
+while ( WiFi.status() != WL_CONNECTED ) {
+delay ( 500 );
+}
+timeClient.begin();
 }
 
 void loop() {
+timeClient.update();
 
-h=12;
-m=34;
-s=56;
-
- if (millis() - timing > 1000){ 
- timing = millis(); 
- points.attach(1, blink_points);
- }
+if (millis() - timing > 1000){ 
+timing = millis(); 
+points.attach(1, blink_points);
+}
   
- tm1637.point(point);
- TimeDisp[0] = h / 10;                                          
- TimeDisp[1] = h % 10;            
- TimeDisp[2] = m / 10;         
- TimeDisp[3] = m % 10;   
- TimeDisp[4] = s / 10;         
- TimeDisp[5] = s % 10;      
- tm1637.display(TimeDisp);         
-                            
+tm1637.point(point);
+TimeDisp[0] = timeClient.getHours() / 10;                                          
+TimeDisp[1] = timeClient.getHours() % 10;            
+TimeDisp[2] = timeClient.getMinutes() / 10;         
+TimeDisp[3] = timeClient.getMinutes() % 10;   
+TimeDisp[4] = timeClient.getSeconds() / 10;         
+TimeDisp[5] = timeClient.getSeconds() % 10;      
+tm1637.display(TimeDisp);         
 }
 
 void blink_points(){ 
-  point = !point;
- } 
-
+point = !point;
+} 
